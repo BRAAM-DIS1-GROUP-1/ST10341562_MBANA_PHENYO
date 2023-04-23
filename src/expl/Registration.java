07960aaf508787b7b7fed2671a91715fdcf51d85
phenyo <130745192+PhenyoMbana@users.noreply.github.com>
2023-04-23 18:40:36 +0000

    Add files via upload

---
+++ src/expl/Registration.java
@@ -0,0 +1,54 @@
+package expl;
+
+import java.util.Scanner;
+
+public class Registration{
+
+    public static void main(String[] args) {
+        
+        // Account REGISTRATION
+        System.out.println("Welcome to Registration!");
+        
+  // Create a new object to read input from the console
+        Scanner scanner = new Scanner(System.in);
+       // Ask the user for their desired username
+        String username;
+        while (true) {
+            System.out.println("Enter username");
+            username = scanner.nextLine();
+              // Check if the username meets our requirements
+            if (username.length() <= 5 && username.contains("_")) {
+                System.out.println("Username successfully captured.");
+                break;  // Exit the loop if the username is valid
+            } else {
+                // If the username is not valid, ask the user to try again
+                System.out.println("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.");
+            }
+        }
+
+        // Ask the user to create a password
+        String password;
+        while (true) {
+            System.out.println("Enter password");
+            password = scanner.nextLine();
+             // Check if the password meets our requirements
+            if (password.length() >= 8 && password.matches(".*[A-Z].*") && password.matches(".*\\d.*") && password.matches(".*[!@#$%^&*()].*")) {
+                System.out.println("Password successfully captured.");
+                break; // Exit the loop if the password is valid
+            } else {
+                // If the password is not valid, ask the user to try again
+                System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number, and a special character.");
+            }
+        }
+ // Ask the user for their first and last name
+        System.out.println("Enter first name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter last name: ");
+        String lastName = scanner.nextLine();
+
+       
+
+        // Confirm the user's registration
+        System.out.println("Registration successful. Welcome " + firstName + " " + lastName + "!");
+    }
+}
